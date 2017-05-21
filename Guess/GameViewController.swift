95ec6645c35f00a877bbf48109ade6c4bb9245db
james-sutt <james.abcde123@gmail.com>
2017-05-21 16:32:17 -0700

    Add comments, shorten the code

--- Guess/GameViewController.swift
+++ Guess/GameViewController.swift
@@ -21,6 +21,7 @@ enum Status {
 
 class GameViewController: UIViewController {
 
+    // MARK: Variables
     var words: [String]!
     var flags = [Bool]() // true = done with that word
     var timer: Timer!
@@ -39,6 +40,8 @@ class GameViewController: UIViewController {
         return count % words.count
     }
     
+    
+    // MARK: Constants
     let green = UIColor.init(red: 180/255.0, green: 232/255.0, blue: 112/255.0, alpha: 1.0)
     let red = UIColor.init(red: 255/255.0, green: 69/255.0, blue: 69/255.0, alpha: 1.0)
     let blue = UIColor.init(red: 56/255.0, green: 189/255.0, blue: 255/255.0, alpha: 1.0)
@@ -49,10 +52,12 @@ class GameViewController: UIViewController {
     let motionUpdateInterval = 0.3
     let timerTolerance = 0.05
     
-    
+    // MARK: Outlets
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var wordLabel: UILabel!
     
+    
+    // MARK: UIViewController overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -77,6 +82,8 @@ class GameViewController: UIViewController {
         status = .readyToBegin
     }
     
+    // MARK: Motion
+    
     func motionUpdate(roll: Double, pitch: Double) {
         if pitch > -0.2 && pitch < 0.2 {
             if roll > 1.2 && roll < 1.9 {
@@ -86,32 +93,22 @@ class GameViewController: UIViewController {
                 } else if status == .readyToContinue {
                     nextWord()
                 }
-            } else if roll > 2.35 && roll < 3.14 {
-                // Correct
-                if status == .display {
-                    correct()
-                }
-            } else if roll > 0 && roll < 0.78 {
-                // Pass
-                if status == .display {
-                    pass()
-                }
+            } else if roll > 2.35 && roll < 3.14 && status == .display {
+                correct()
+            } else if roll > 0 && roll < 0.78 && status == .display {
+                pass()
             }
         }
         
     }
+    
+    // MARK: Actions
 
     @IBAction func back() {
-        timer?.invalidate()
+        endGame()
         dismiss(animated: false, completion: nil)
     }
-    
-    func setupFlags() {
-        for _ in words {
-            flags.append(false)
-        }
-    }
-    
+ 
     func startGame() {
         status = .getReady
         // start timer, display first word, change status to display
@@ -119,21 +116,17 @@ class GameViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.timeUpdate), userInfo: nil, repeats: true)
         timer.tolerance = timerTolerance
         
-        wordLabel.text = "GET READY"
-        wordLabel.textColor = UIColor.white
-        view.backgroundColor = blue
-        timerLabel.text = String(prepareTime)
-        timerLabel.textColor = UIColor.white
+        configureWord(text: "GET READY", color: UIColor.white)
+        configureTimer(time: prepareTime, color: UIColor.white)
         timerLabel.isHidden = false
+        view.backgroundColor = blue
     }
     
     func showFirstWord() {
         status = .display
-        wordLabel.text = words[index]
-        wordLabel.textColor = UIColor.black
+        configureWord(text: words[index], color: UIColor.black)
+        configureTimer(time: gameTime, color: UIColor.lightGray)
         view.backgroundColor = UIColor.white
-        timerLabel.textColor = UIColor.lightGray
-        timerLabel.text = String(gameTime)
     }
     
     func nextWord() {
@@ -142,21 +135,18 @@ class GameViewController: UIViewController {
             count += 1;
             print(index)
         } while (flags[index])
-        wordLabel.text = words[index]
-        wordLabel.textColor = UIColor.black
+        configureWord(text: words[index], color: UIColor.black)
         view.backgroundColor = UIColor.white
     }
     
     func correct() {
         correctSoundPlayer.play()
-        wordLabel.text = "Correct"
-        wordLabel.textColor = UIColor.white
+        configureWord(text: "Correct", color: UIColor.white)
         view.backgroundColor = green
         flags[index] = true
         score += 1
         if score == words.count {
-            timer.invalidate()
-            status = .end
+            endGame()
             showResult()
         } else {
             status = .readyToContinue
@@ -166,29 +156,8 @@ class GameViewController: UIViewController {
     func pass() {
         incorrectSoundPlayer.play()
         status = .readyToContinue
-        wordLabel.text = "Pass"
-        wordLabel.textColor = UIColor.white
+        configureWord(text: "Pass", color: UIColor.white)
         view.backgroundColor = red
-    }
-    
-
-    func timeUpdate() {
-        if prepareTime > 0 {
-            prepareTime -= 1
-            timerLabel.text = String(prepareTime)
-            if prepareTime == 0 {
-                showFirstWord()
-            }
-        } else {
-            gameTime -= 1
-            print(gameTime)
-            timerLabel.text = String(gameTime)
-            if gameTime == 0 {
-                timer.invalidate()
-                status = .end
-                showResult()
-            }
-        }
     }
     
     func showResult() {
@@ -202,6 +171,47 @@ class GameViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func endGame() {
+        timer?.invalidate()
+        motionManager?.stopDeviceMotionUpdates()
+        status = .end
+    }
+    
+    // MARK: Timer callback
+    func timeUpdate() {
+        if prepareTime > 0 {
+            prepareTime -= 1
+            timerLabel.text = String(prepareTime)
+            if prepareTime == 0 {
+                showFirstWord()
+            }
+        } else {
+            gameTime -= 1
+            print(gameTime)
+            timerLabel.text = String(gameTime)
+            if gameTime == 0 {
+                endGame()
+                showResult()
+            }
+        }
+    }
+    
+    // MARK: Other
+    func setupFlags() {
+        for _ in words {
+            flags.append(false)
+        }
+    }
+    
+    func configureWord(text: String, color: UIColor) {
+        wordLabel.text = text
+        wordLabel.textColor = color
+    }
+    
+    func configureTimer(time: Int, color: UIColor) {
+        timerLabel.text = String(time)
+        timerLabel.textColor = color
+    }
     
 }
 
